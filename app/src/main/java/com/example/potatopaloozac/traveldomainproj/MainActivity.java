02caa987c632f985251720b9852d66a808ac5ac8
wang- <1715088795@qq.com>
2018-10-06 14:19:24 -0500

    put anychart

--- app/src/main/java/com/example/potatopaloozac/traveldomainproj/MainActivity.java
+++ app/src/main/java/com/example/potatopaloozac/traveldomainproj/MainActivity.java
@@ -14,7 +14,12 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
+import com.anychart.charts.Cartesian;
+import com.anychart.charts.CircularGauge;
+import com.anychart.charts.Pie;
 import com.anychart.charts.TagCloud;
+import com.anychart.core.gauge.pointers.Bar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,14 +39,40 @@ public class MainActivity extends BaseActivity {
 
         List<DataEntry> dataEntryList = new ArrayList<>();
 
-        dataEntryList.add(new ValueDataEntry("myname0", 123));
-        dataEntryList.add(new ValueDataEntry("myname1", 456));
-        dataEntryList.add(new ValueDataEntry("myname2", 789));
-        dataEntryList.add(new ValueDataEntry("myname3", 12));
-        dataEntryList.add(new ValueDataEntry("myname4", 345));
+        dataEntryList.add(new ValueDataEntry("Atlanta", 51000));
+        dataEntryList.add(new ValueDataEntry("Philadelphia", 43000));
 
-        TagCloud tagCloud = AnyChart.tagCloud();
-        tagCloud.data(dataEntryList);
-        anyChartView.setChart(tagCloud);
+        dataEntryList.add(new ValueDataEntry("New York City", 33000));
+        dataEntryList.add(new ValueDataEntry("Dallas", 25000));
+
+        dataEntryList.add(new ValueDataEntry("Washington", 20000));
+
+        dataEntryList.add(new ValueDataEntry("Denver", 14000));
+        dataEntryList.add(new ValueDataEntry("st Charles", 25));
+        //st charles, 25000
+        //new york city, 33 million
+        //dallas, 25 million
+        //atlanta, 51 million
+        //washington, 20 million
+        //Philadelphia, 43 million
+        //Denver, 14 million
+
+
+//        Cartesian cartesian = AnyChart.cartesian();
+//        cartesian.data(dataEntryList);
+//        anyChartView.setChart(cartesian);
+
+//        Pie pie = AnyChart.pie();
+//        pie.data(dataEntryList);
+//        anyChartView.setChart(pie);
+
+        Cartesian bar = AnyChart.bar();
+        bar.data(dataEntryList);
+        bar.title().enabled();
+        bar.title("Most Popular Destinations");
+
+        anyChartView.setChart(bar);
+
+
     }
 }
